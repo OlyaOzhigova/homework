8d2903e516181f62937fd723e021e5208fb2cb9e
OlyaOzhigova <olya.ozhigova@gmail.com>
2025-10-08 15:19:09 +0000

    User(AbstractUser)

--- diplom/diplom_retail_network/backend/models.py
+++ diplom/diplom_retail_network/backend/models.py
@@ -76,7 +76,7 @@ class Category(models.Model):
 
     class Meta:
         verbose_name = 'Категория'
-        verbose_name_plural = "Список категорий"
+        verbose_name_plural = "Список категорий  "
 
     def __str__(self):
         return self.name
