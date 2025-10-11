989569279aac1a2d387f7a8e1abea213dbfaef80
Ольга <155555390+OlyaOzhigova@users.noreply.github.com>
2025-10-11 23:10:09 +0700

    Update settings.py
    обновление

--- diplom/diplom_retail_network/netology_pd_diplom/settings.py
+++ diplom/diplom_retail_network/netology_pd_diplom/settings.py
@@ -78,7 +78,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-AUTH_USER_MODEL = 'backend.User'  # Пока закомментировано
+AUTH_USER_MODEL = 'backend.User'  #добавила
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
